8bcd7fae9a04e5af730dc39077350a287469bc4f
SuguruIrie3224 <121938178+SuguruIrie3224@users.noreply.github.com>
2024-10-22 19:24:45 +0900

    コピーに変更

--- .github/move-ref.ps1
+++ .github/move-ref.ps1
@@ -1,2 +1,1 @@
-Move-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\" -Force
-Remove-Item -Path "C:\Users\suguru irie\ProjectBlueArchive\Recordings\*.mp4" -Force
+Copy-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\"
