de4027fc80fdb0255adaf8f247c2e05347ff57e7
SuguruIrie3224 <121938178+SuguruIrie3224@users.noreply.github.com>
2024-10-22 19:18:11 +0900

    コマンドの切り出し

---
+++ .github/move-ref.ps1
@@ -0,0 +1,2 @@
+Move-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\" -Force
+Remove-Item -Path "C:\Users\suguru irie\ProjectBlueArchive\Recordings\*.mp4" -Force
