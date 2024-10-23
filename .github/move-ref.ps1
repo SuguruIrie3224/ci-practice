419ab4d6f514d338dba6f19ddb5f89d8a27aa196
SuguruIrie3224 <121938178+SuguruIrie3224@users.noreply.github.com>
2024-10-23 19:39:33 +0900

    書き出しからアップロードまで

--- .github/move-ref.ps1
+++ .github/move-ref.ps1
@@ -1,2 +1,8 @@
 # $PSDefaultParameterValues['*:Encording'] = 'utf8'
-Copy-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\"
+# 引数の受け取り
+param(
+    [int]$pullnumber,
+)
+
+New-Item -ItemType Directory -Path "G:\\マイドライブ\\Share\\${pullnumber}" -Force
+Copy-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\${pullnumber}\\"
