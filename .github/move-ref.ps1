5af33f40f4a750ff0d57a6d8b836991de910ccdb
SuguruIrie3224 <121938178+SuguruIrie3224@users.noreply.github.com>
2024-10-24 15:36:13 +0900

    ファイル移動のフローの調整

--- .github/move-ref.ps1
+++ .github/move-ref.ps1
@@ -6,3 +6,4 @@ param(
 
 New-Item -ItemType Directory -Path "G:\\マイドライブ\\Share\\${pullnumber}" -Force
 Copy-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Destination "G:\\マイドライブ\\Share\\${pullnumber}\\"
+Remove-Item -Path "C:\\Users\\suguru irie\\ProjectBlueArchive\\Recordings\\*.mp4" -Force
